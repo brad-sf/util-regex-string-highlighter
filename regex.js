f69205cfd75a023229276dc301702ab6ca980b66
Bradley Flood <bradley.flood@gmail.com>
2017-04-10 11:55:37 +1000

    Simplify

--- regex.js
+++ regex.js
@@ -1,8 +1,6 @@
 const chalk = require('chalk');
-const printer = (str, regex) => {
+const printer = (str, regex) =>
     console.log(str.replace(regex, str => chalk.bgBlue(str)));
-}
 
-let str = "This is my test String!";
-let regex = /is/;
-printer(str,regex);
+printer("This is my test", /is/);
+
